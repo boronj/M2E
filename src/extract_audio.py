3d18fe360cc354c7b725b2c955cc1fab2ed29939
boronj <boronj@proton.me>
2025-09-18 21:42:01 -0500

    Setting up setup.cfg to work w submodules + import requirements

--- src/extract_audio.py
+++ src/extract_audio.py
@@ -3,7 +3,6 @@ import sys
 sys.dont_write_bytecode = True
 
 #Other dependencies
-from . import error_handling #Fixing error
 from datasets import *
 from error_handling import throwError
 from colorama import Fore, Style
