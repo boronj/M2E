9fa8805d0676543f5ff0821df8a3d2a696e2a0ea
Jacob Boron <boronj@proton.me>
2025-09-27 06:14:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
 		"nvidia-nccl-cu12==2.27.3"
 	],
 	package_dir = {'':'src'},
-	packages=['m2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
+	packages=['m2e', 'm2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
 )
