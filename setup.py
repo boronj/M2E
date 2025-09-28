576118b3fd4628111967af008821beaccdc0e74c
Jacob Boron <boronj@proton.me>
2025-09-27 19:30:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
 		"numpy==2.0.0",
 		"nvidia-nccl-cu12==2.27.3",
 		"matplotlib==2.1.0",
+		"h5py==3.5.0"
 	],
 	package_dir = {'':'src'},
 	packages=['m2e', 'm2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
