9fb0104b40c3dd6ff531bdb10d47d6786875bd3a
Jacob Boron <boronj@proton.me>
2025-09-27 20:09:01 -0500

    fixing setup.py pt 25

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
 		"matplotlib==3.10.0",
 		"h5py==3.14.0"
 	],
-	package_dir = {'':'src'},
+	package_dir = {'m2e':'src/m2e/', 'm2e.extract_audio':'src/m2e/extract_audio/', 'm2e.analyze_video':'src/m2e/analyze_video', 'm2e.error_handling': 'src/m2e/error_handling'},
 	packages=['m2e', 'm2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
 )
