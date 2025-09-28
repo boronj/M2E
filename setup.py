7fe48f85cad8f75239affb4f65e1ec729c1f9e96
Jacob Boron <boronj@proton.me>
2025-09-27 21:02:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
 		"h5py==3.14.0"
 	],
 	packages=["m2e", "m2e.extract_audio", "m2e.analyze_video", "m2e.error_handling"],
-	package_dir = {'m2e':'src/m2e/', 'm2e.extract_audio': '/src/m2e/extract_audio', 'm2e.analyze_video': '/src/m2e/analyze_video', 'm2e.error_handling':'/src/m2e/error_handling'},
+	package_dir = {'m2e':'src/m2e/', 'm2e.extract_audio': 'extract_audio', 'm2e.analyze_video': '/analyze_video', 'm2e.error_handling':'error_handling'},
 
 )
