178054df754f91bad7ef4aed2bebed3b595ab096
Jacob Boron <boronj@proton.me>
2025-09-27 20:28:47 -0500

    .

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
 		"matplotlib==3.10.0",
 		"h5py==3.14.0"
 	],
-	package_dir = {'m2e':'src/m2e/', 'm2e.extract_audio':'src/m2e/extract_audio/', 'm2e.analyze_video':'src/m2e/analyze_video', 'm2e.error_handling': 'src/m2e/error_handling'},
-	packages=['m2e', 'm2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
+	packages=["m2e"],
+	package_dir = {'m2e':'src/m2e/'},
+
+	py_modules=['m2e', 'm2e.extract_audio', 'm2e.analyze_video', 'm2e.error_handling']
 )
