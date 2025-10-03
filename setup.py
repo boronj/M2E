3a784dfab0cbfc970b011cd359856fb600ec57c3
Jacob Boron <boronj@proton.me>
2025-10-03 00:36:23 -0500

    adding torchcodec dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
 		"numpy==2.0.0",
 		"nvidia-nccl-cu12==2.27.3",
 		"matplotlib==3.10.0",
-		"h5py==3.14.0"
+		"h5py==3.14.0",
+		"torchcodec"
 	],
 	packages=["m2e", "m2e.extract_audio", "m2e.analyze_video", "m2e.error_handling"],
 	package_dir = {'':'src'},
