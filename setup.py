8bc6cd97df7a4af9fd751fd977af49ab13aa3229
Jacob Boron <boronj@proton.me>
2025-10-05 20:34:13 -0500

    removing VOCA-specific reqs

--- setup.py
+++ setup.py
@@ -11,11 +11,7 @@ setup(
 		"soundfile",
 		"ffmpeg",
 		"datasets[audio] == 3.6.0",
-		"colorama",
-		"numpy==2.0.0",
-		"matplotlib==3.10.0",
-		"h5py==3.14.0",
-		"torchcodec==0.5.0"
+		"colorama"
 	],
 	packages=["m2e", "m2e.extract_audio", "m2e.analyze_video", "m2e.error_handling"],
 	package_dir = {'':'src'},
