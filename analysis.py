4e1fd4ee589480a953b0200840cfa60410805ad9
Jacob Boron <boronj@proton.me>
2026-02-07 01:52:23 -0600

    Setup up analysis file

---
+++ analysis.py
@@ -0,0 +1,8 @@
+import glob, argparse
+import pandas as pd
+
+#Take GEMAPS table for each file and add it to its respective AU summaries row 
+
+#Run regression(?) analysis
+
+#main() sets up directory to point to
