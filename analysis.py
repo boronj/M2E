0539bf640d54d6dbed192aa081bc417c3462544c
Jacob Boron <boronj@proton.me>
2026-02-08 22:26:31 -0600

    Work on combining GEMAPS/AU tables together

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import glob, argparse, os
 import pandas as pd
+from tqdm import tqdm
 from colorama import Fore, Style
 from m2e.error_handling import throw_error
 
@@ -10,15 +11,25 @@ def combineTables(gemaps_directory, au_tables_path):
   except Exception as e:
     throw_error("dataset failed to load", e)
   else:
-    for i in df.index:
+    #Create columns in au_dataset to support GEMAPS columns
+    gemaps_column_names = list(pd.read_csv(gemaps_directory[0]).columns)
+    for column in tqdm(gemaps_column_names, desc = "Adding GEMAPS columns: ", total = len(gemaps_column_names)):
+      au_dataset[column] = [0]*len(au_dataset)
+
+    #Merge values into au_dataset 
+    for i in tqdm(au_dataset.index, desc = "Combining datasets: ", total = len(au_dataset.index)):
       #Check in gemaps_directory for an equivalent file
-
-      #If it is in au_dataset, access the values from that file
-
-      #Load them one-by-one into au_dataset
+      video_file = str(au_dataset.at[i, "video"]) #.../.../.../xxx.csv
+      
+      if video_file.replace(".csv",".mp4") in gemaps_directory:
+        #If it is in au_dataset, access the values from that file
+        gemaps_dataset = pd.read_csv(video_file)
+        assert len(list(gemaps_dataset.columns)) == len(gemaps_column_names), "Amount of columns in GEMAPS dataset doesn't align" #Idk if this will actually go through or not and I don't want to check manually
+        #Load them one-by-one into au_dataset
+        for column in tqdm(list(gemaps_dataset.columns), desc = "Combining columns: ", total = len(list(gemaps_dataset.columns))):
+          au_dataset.at[i, column] = gemaps_dataset[0][column]
 
     #Return the new AU dataset, and also export it into a new thing
-    #Include checks to make sure # of videos in === # of videos out
 
 #Run regression(?) analysis
 def PLSRegression():
