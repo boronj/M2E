21677c6622160a0323dd3173ea8456cf6e6d4269
Jacob Boron <boronj@proton.me>
2026-02-13 01:02:14 -0600

    Add Spearman correlation

--- analysis.py
+++ analysis.py
@@ -7,11 +7,14 @@ from m2e.error_handling import throw_error
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from scipy.stats import spearmanr
 
 MEASURED_METRIC = "loudness_sma3_amean"
+gemaps_columns = []
 
 #Take GEMAPS table for each file and add it to its respective AU summaries row 
 def combine_tables(gemaps_directory, gemaps_tables_path, au_tables_path, output_path):
+  global gemaps_columns
   try:
     au_dataset = pd.read_csv(au_tables_path)
   except Exception as e:
@@ -19,11 +22,10 @@ def combine_tables(gemaps_directory, gemaps_tables_path, au_tables_path, output_
   else:
     #Create columns in au_dataset to support GEMAPS columns
     gemaps_column_names = list(pd.read_csv(gemaps_directory[0]).columns)
+    gemaps_columns = gemaps_column_names
     for column in tqdm(gemaps_column_names, desc = "Adding GEMAPS columns: ", total = len(gemaps_column_names)):
       au_dataset[column] = [0]*len(au_dataset)
-
-    print(gemaps_directory)
-      
+        
     #Merge values into au_dataset 
     for i in tqdm(au_dataset.index, desc = "Combining datasets: ", total = len(au_dataset.index)):
       #Check in gemaps_directory for an equivalent file
@@ -43,21 +45,28 @@ def combine_tables(gemaps_directory, gemaps_tables_path, au_tables_path, output_
     return au_dataset
 
 #Run PLS regression model + analysis
-
-#TO_DO: Check through dataframe (n_components is apparently set to a max 1 row, this shouldn't be the case so smth's off)
-        #Figure out if *all* AU data instead of means should be put in (this isn't that hard of a change but does require another 2.5 hours of time)
-        #Check if using linear regression would work better than PLS
-        #Everything also runs to 0 for some reason so check for that as well
-
 def pls_regression(x_axis, y_axis):
-  x_train, x_test, y_train, y_test = train_test_split(x_axis, y_axis, test_size = 0.2, random_state=42)
+  print(x_axis.shape)
+  print(x_axis.head)
+  #Use Spearman correlation(?) to determine colinearity (and to see whether or not using a LinReg model would suck)
+  spearman, p_value = spearmanr(x_axis, y_axis)
+  print(f"Spearman constant: {spearman}")
+    
+  #Split test/train
+  x_train, x_test, y_train, y_test = train_test_split(x_axis, y_axis, test_size = 0.5, random_state=42)
+    
+  #Calculate RMSEP to determine how many components to use for PLS regression
+
+  #Set up PLSRegression model
   pls_model = PLSRegression(n_components=1, scale=True)
   pls_model.fit(x_train, y_train)
   y_predicted = pls_model.predict(x_test)
+
+  #Find MSE, coefficient(s) on n_component variable(s), and R^2
   mse = mean_squared_error(y_test, y_predicted)
   coefficients = pls_model.coef_
-  r2 = r2_score(y_test, y_predicted)
-  return (mse, coefficients, r2)
+  r2 = pls_model.score(x_test, y_test)
+  return (mse, coefficients, r2, spearman)
 
 #main() sets up directory to point to
 def main():
@@ -75,7 +84,8 @@ def main():
     dataset = combine_tables(gemaps_tables, args.gemaps_tables, f"{args.au_tables}/summaries.csv", args.output_path)
   else:
     raise RuntimeError(f"AU summary table doesn't exist at '{args.au_tables}/summaries.csv'")
-    
+
+  print(gemaps_columns)
   au_cols = [x for x in dataset.columns if "AU" in x]
   au_results = {}
   #Run PLS regression on a specifed column/acoustic cue
@@ -84,14 +94,15 @@ def main():
     au_results[au.split("_")[0]] = {
         "MSE": regression_model_results[0],
         "Coefficients": regression_model_results[1],
-        "R^2": regression_model_results[2]
+        "R^2": regression_model_results[2],
+        "Spearman": regression_model_results[3]
     }
       
   #Display results
-  print(f"{'AU':<6} {'MSE':<6} {'Coeffs':<20} {'R^2':<10}")
+  print(f"{'AU':<6} {'MSE':<6} {'Coeffs':<20} {'R^2':<10} {'Spearman':<5}")
   print('-'*42)
   for col in au_results:
-       print(f"{col:<6} {au_results[col]['MSE']:<6.2f} {au_results[col]['MSE']:<6.2f} {au_results[col]['R^2']:<6.2f}")
+       print(f"{col:<6} {au_results[col]['MSE']:<6.2f} {str(au_results[col]['Coefficients']):<20} {au_results[col]['R^2']:<10.2f} {au_results[col]['Spearman']:<5.2f}")
 
 
 if __name__ == "__main__":
